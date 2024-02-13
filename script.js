1077cef023fe485fe6f3c2eb8c5247206b5621df
huinyacode <nikitakharlov007@gmail.com>
2024-02-13 21:23:07 +0600

    ready

---
+++ script.js
@@ -0,0 +1,25 @@
+const no = document.getElementById("no");
+const quest = document.getElementById("q");
+const greet = document.getElementById("g");
+const yes = document.getElementById("yes");
+const img = document.getElementById("img");
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+no.addEventListener("mouseover", () => {
+    img.src = "./images/cat_lose.gif"
+    img.style = "height: 250px;"
+    no.style = `left: ${getRandomArbitrary(1,9) * 10}%;
+                top: ${getRandomArbitrary(3,9) * 10}%`;
+})
+
+yes.addEventListener("click", ()=>{
+    img.src = "./images/cat_love.gif";
+    img.style = "transform: scale(2); position: relative; top: 10%"
+    quest.classList.toggle("not")
+    greet.classList.toggle("not")
+    no.classList.toggle("not");
+    yes.classList.toggle("not");
+})
