ff3d869bcba998db9ef6c6388a2eb0f587280b9e
huinyacode <nikitakharlov007@gmail.com>
2024-02-13 22:31:13 +0600

    rr

--- script.js
+++ script.js
@@ -15,9 +15,18 @@ no.addEventListener("mouseover", () => {
                 top: ${getRandomArbitrary(3,9) * 10}%`;
 })
 
+no.addEventListener("click", () => {
+    img.src = "./images/cat_lose.gif"
+    
+    img.style = "height: 250px; position: relative; left: 8%; top: 10%"
+    no.style = `left: ${getRandomArbitrary(1,9) * 10}%;
+                top: ${getRandomArbitrary(3,9) * 10}%`;
+})
+
 yes.addEventListener("click", ()=>{
     img.src = "./images/cat_love.gif";
-    img.style = "transform: scale(2); position: relative; top: 10%"
+    img.style = "transform: scale(1.7); position: relative; top: 10%; left: 4.5%";
+    img.classList.toggle("love");
     quest.classList.toggle("not")
     greet.classList.toggle("not")
     no.classList.toggle("not");
